48459cc47c0cf8eb07305cfb13ae24ba426e7a86
Pxpe123 <114689870+Pxpe123@users.noreply.github.com>
2024-02-03 11:33:55 +0000

    Update dealership page layout and add a button to request data update

--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,92 @@
 const serverUrl = "141.145.202.202:3002";
-const MainBody = document.getElementById('PagesBody');
-const loadingScreen = document.getElementById('loadingScreen');
-const myModal = document.getElementById('myModal');
+const MainBody = document.getElementById("PagesBody");
+const loadingScreen = document.getElementById("loadingScreen");
+const myModal = document.getElementById("myModal");
 
 const pages = {
-    'home':       { btn: document.getElementById('homePageBtn'),       path: './pages/home.html',           setup: setupHome },
-    'user':       { btn: document.getElementById('userPageBtn'),       path: './pages/user.html',           setup: setupUserInfo },
-    'server':     { btn: document.getElementById('serverPageBtn'),     path: './pages/server.html',         setup: null },
-    'storage':    { btn: document.getElementById('storagePageBtn'),    path: './pages/storage.html',        setup: null },
-    'dealership': { btn: document.getElementById('dealershipPageBtn'), path: './pages/dealership.html',     setup: setupDealershipPage},    
-    'settings':   { btn: document.getElementById('settingsPageBtn'),   path: './pages/modal/settings.html', setup: null }
+  home: {
+    btn: document.getElementById("homePageBtn"),
+    path: "./pages/home.html",
+    setup: setupHome,
+  },
+  user: {
+    btn: document.getElementById("userPageBtn"),
+    path: "./pages/user.html",
+    setup: setupUserInfo,
+  },
+  server: {
+    btn: document.getElementById("serverPageBtn"),
+    path: "./pages/server.html",
+    setup: null,
+  },
+  storage: {
+    btn: document.getElementById("storagePageBtn"),
+    path: "./pages/storage.html",
+    setup: null,
+  },
+  dealership: {
+    btn: document.getElementById("dealershipPageBtn"),
+    path: "./pages/dealership.html",
+    setup: setupDealershipPage,
+  },
+  settings: {
+    btn: document.getElementById("settingsPageBtn"),
+    path: "./pages/modal/settings.html",
+    setup: null,
+  },
 };
 
-let activePage = 'home';
- 
-function loadPage(page) {
-    if (activePage === page) {
-        event.preventDefault();
-        return;
-    }
+let activePage = "home";
 
-    loadingScreen.classList.toggle('loadingScreen-hide');
-    fetch(pages[page].path)
-        .then(response => response.text())
-        .then(data => {
-            setTimeout(() => {
-                if (myModal.style.display == "block") {
-                    myModal.style.display = "none";
-                }
-                MainBody.innerHTML = data;
-                loadingScreen.classList.toggle('loadingScreen-hide');
-                if (pages[page].setup) {
-                    pages[page].setup();
-                }
-                activePage = page;
-            }, 300);
-        });
+function loadPage(page) {
+  if (activePage === page) {
+    event.preventDefault();
+    return;
+  }
+
+  loadingScreen.classList.toggle("loadingScreen-hide");
+  fetch(pages[page].path)
+    .then((response) => response.text())
+    .then((data) => {
+      setTimeout(() => {
+        if (myModal.style.display == "block") {
+          myModal.style.display = "none";
+        }
+        MainBody.innerHTML = data;
+        loadingScreen.classList.toggle("loadingScreen-hide");
+        if (pages[page].setup) {
+          pages[page].setup();
+        }
+        activePage = page;
+      }, 300);
+    });
 }
 
 function setupPageListeners() {
-    for (const page in pages) {
-        pages[page].btn.addEventListener('click', event => loadPage(page));
-    }
+  for (const page in pages) {
+    pages[page].btn.addEventListener("click", (event) => loadPage(page));
+  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    setupPageListeners();
-    InitialSetup();
+document.addEventListener("DOMContentLoaded", function () {
+  setupPageListeners();
+  InitialSetup();
 });
 
 async function InitialSetup() {
-    try {
-        const response = await fetch(pages.dealership.path);
-        const data = await response.text();
-        MainBody.innerHTML = data;
-        await new Promise(resolve => setTimeout(resolve, 300));
-        loadingScreen.classList.toggle('loadingScreen-hide');
-        setupHome();
-        activePage = 'home';
-    } catch (error) {
-        console.error("Error during InitialSetup:", error);
-    }
+  try {
+    const response = await fetch(pages.home.path);
+    const data = await response.text();
+    MainBody.innerHTML = data;
+    await new Promise((resolve) => setTimeout(resolve, 300));
+    loadingScreen.classList.toggle("loadingScreen-hide");
+    setupHome();
+    activePage = "home";
+  } catch (error) {
+    alert(
+      "Error during InitialSetup:",
+      error,
+      "Please Restart the app or Contance @Peps1co"
+    );
+  }
 }
--- src/js/pages/dealership.js
+++ src/js/pages/dealership.js
@@ -1,109 +1,147 @@
 var response;
 
 async function setupDealershipPage() {
-    await getDealershipJson();
-    CreateDynamicBoxes();
+  await getDealershipJson();
+  CreateDynamicBoxes();
+  SetDealershiptitle();
 }
 
 setupDealershipPage();
 
 async function getDealershipJson() {
-    var userdataURL = "http://141.145.202.202:3001/Dealership";
-    const fetchOptions = {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    };
-    response = await fetch(userdataURL, fetchOptions);
-    response = await response.json();
+  var userdataURL = "http://141.145.202.202:3001/Dealership";
+  const fetchOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  response = await fetch(userdataURL, fetchOptions);
+  response = await response.json();
 }
 
-let timestampValue; 
+let timestampValue;
 
 function CreateDynamicBoxes() {
-    Object.keys(response).forEach(function (category) {
-        if (category === "timestamp") {
-            // If the category is timestamp, save the value
-            timestampValue = response[category];
-            return;
-        }
+  Object.keys(response).forEach(function (category) {
+    if (category === "timestamp") {
+      // If the category is timestamp, save the value
+      timestampValue = response[category];
+      return;
+    }
 
-        // Skip if response[category] is not an array
-        if (!Array.isArray(response[category])) {
-            console.error("Invalid data structure for category:", category);
-            return;
-        }
+    // Skip if response[category] is not an array
+    if (!Array.isArray(response[category])) {
+      console.error("Invalid data structure for category:", category);
+      return;
+    }
 
-        var dropdownContainer = document.createElement("div");
-        dropdownContainer.classList.add("dropdown");
-        var dropdownId = "dropdown" + (Object.keys(response).indexOf(category) + 1);
-        dropdownContainer.id = dropdownId;
-        dropdownContainer.setAttribute("onclick", "toggleDropdown('" + dropdownId + "')");
+    var dropdownContainer = document.createElement("div");
+    dropdownContainer.classList.add("dropdown");
+    var dropdownId = "dropdown" + (Object.keys(response).indexOf(category) + 1);
+    dropdownContainer.id = dropdownId;
+    dropdownContainer.setAttribute(
+      "onclick",
+      "toggleDropdown('" + dropdownId + "')"
+    );
 
-        var dropdownHeader = document.createElement("h3");
-        dropdownHeader.innerHTML = category;
-        var arrowIcon = document.createElement("i");
-        arrowIcon.classList.add("fa-solid", "fa-arrow-down");
-        dropdownHeader.appendChild(arrowIcon);
+    var dropdownHeader = document.createElement("h3");
+    dropdownHeader.innerHTML = category;
+    var arrowIcon = document.createElement("i");
+    arrowIcon.classList.add("fa-solid", "fa-arrow-down");
+    dropdownHeader.appendChild(arrowIcon);
 
-        var adropdownContainer = document.createElement("div");
-        adropdownContainer.classList.add("adropdown");
-        var adropdownId = "adropdown" + (Object.keys(response).indexOf(category) + 1);
-        adropdownContainer.id = adropdownId;
+    var adropdownContainer = document.createElement("div");
+    adropdownContainer.classList.add("adropdown");
+    var adropdownId =
+      "adropdown" + (Object.keys(response).indexOf(category) + 1);
+    adropdownContainer.id = adropdownId;
 
-        dropdownContainer.appendChild(dropdownHeader);
-        dropdownContainer.appendChild(adropdownContainer);
+    dropdownContainer.appendChild(dropdownHeader);
+    dropdownContainer.appendChild(adropdownContainer);
 
-        document.getElementById("dealershipInnerPage").appendChild(dropdownContainer);
+    document
+      .getElementById("dealershipInnerPage")
+      .appendChild(dropdownContainer);
 
-        response[category].forEach(function (car) {
-            var vehicleDiv = document.createElement("div");
-            vehicleDiv.classList.add("vehicleDiv");
+    response[category].forEach(function (car) {
+      var vehicleDiv = document.createElement("div");
+      vehicleDiv.classList.add("vehicleDiv");
 
-            var vehicleImage = document.createElement("img");
-            vehicleImage.classList.add("vehicleImage");
-            vehicleImage.src = car.Image;
-            vehicleImage.alt = car.name + " " + car.model;
+      var vehicleImage = document.createElement("img");
+      vehicleImage.classList.add("vehicleImage");
+      vehicleImage.src = car.Image;
+      vehicleImage.alt = car.name + " " + car.model;
 
-            var carNameHeader = document.createElement("h3");
-            carNameHeader.innerHTML = car.name;
+      var carNameHeader = document.createElement("h3");
+      carNameHeader.innerHTML = car.name;
 
-            var modelNameHeader = document.createElement("h6");
-            modelNameHeader.innerHTML = "(" + car.model + ")";
+      var modelNameHeader = document.createElement("h6");
+      modelNameHeader.innerHTML = "(" + car.model + ")";
 
-            var carPriceParagraph = document.createElement("p");
-            carPriceParagraph.innerHTML = FormatPrice(car.price);
+      var carPriceParagraph = document.createElement("p");
+      carPriceParagraph.innerHTML = FormatPrice(car.price);
 
-            vehicleDiv.appendChild(vehicleImage);
-            vehicleDiv.appendChild(carNameHeader);
-            vehicleDiv.appendChild(modelNameHeader);
-            vehicleDiv.appendChild(carPriceParagraph);
+      vehicleDiv.appendChild(vehicleImage);
+      vehicleDiv.appendChild(carNameHeader);
+      vehicleDiv.appendChild(modelNameHeader);
+      vehicleDiv.appendChild(carPriceParagraph);
 
-            adropdownContainer.appendChild(vehicleDiv);
-        });
+      adropdownContainer.appendChild(vehicleDiv);
     });
+  });
+}
+
+function SetDealershiptitle() {
+  const DealershipTitle = document.getElementById("DealershipTitle");
+  var timestamp = response.timestamp;
+  var date = new Date(timestamp);
+  var formattedDateTime = date.toLocaleString();
+  var dealershipText = `Dealership (${formattedDateTime})`;
+  DealershipTitle.innerText = dealershipText;
 }
 
 function toggleDropdown(id) {
-    var dropdown = document.getElementById("a" + id);
+  var dropdown = document.getElementById("a" + id);
 
-    setTimeout(function() {
-        if (dropdown.style.display === "block") {
-            dropdown.style.display = "none";
-        } else {
-            dropdown.style.display = "block";
-        }
-    }, 10);
+  setTimeout(function () {
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
+    } else {
+      dropdown.style.display = "block";
+    }
+  }, 10);
 }
 
 function FormatPrice(price) {
-    const formattedPrice = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 0,
-        maximumFractionDigits: 0,
-    }).format(price);
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
+  }).format(price);
 
-    return formattedPrice;
+  return formattedPrice;
+}
+
+async function requestUpdate() {
+  var userdataURL = "http://141.145.202.202:3001/RequestDealership";
+  const fetchOptions = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  try {
+    const updateResponse = await fetch(userdataURL, fetchOptions);
+    const responseData = await updateResponse.json();
+    console.log(responseData);
+
+    if (responseData.status === "Data Updated") {
+      setupDealershipPage();
+    }
+  } catch (error) {
+    console.error("Error fetching or parsing response:", error);
+  }
 }
